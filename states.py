d1ae809dfa311837aab360dfd9e027a8084a469f
Balazs Orban <balazs.orban@gnd.ro>
2022-09-16 13:02:24 +0300

    Transition to terminal state from visualization service

--- states.py
+++ states.py
@@ -1,29 +1,45 @@
 import os
 
-from fcvisualization import fcvisualization
-
 from FeatureCloud.app.engine.app import AppState, app_state
 
+import fcvisualization
+from threading import Thread
 
-# FeatureCloud requires that apps define the at least the 'initial' state.
-# This state is executed after the app instance is started.
+TERMINAL = False
+
+
+def callback_fn_terminal_state():
+    global TERMINAL
+    print("Transition to terminal state triggered...")
+    TERMINAL = True
+
+
 @app_state('initial')
 class InitialState(AppState):
-
     def register(self):
+        print('registering transition from initial to plot')
         self.register_transition('running')
 
-    def run(self):
+    def run(self) -> str:
+        path_prefix_visualizer = os.getenv("PATH_PREFIX") + 'visualizer/'
+        print("PATH_PREFIX environment variable: ", path_prefix_visualizer)
+        print('Plot start...')
+        fc_visualization = fcvisualization.fcvisualization()
+        thread_vis = Thread(target=fc_visualization.start, args=('fc', path_prefix_visualizer, callback_fn_terminal_state))
+        thread_vis.start()
         return 'running'
+
 
 @app_state('running')
 class RunningState(AppState):
-
     def register(self):
+        print('register transitions from plot state to terminal')
         self.register_transition('running')
+        self.register_transition('terminal')
 
-    def run(self):
-        path_prefix_visualizer = os.getenv("PATH_PREFIX") + 'visualizer/'
-        fc_visualization = fcvisualization()
-        fc_visualization.start('fc', path_prefix_visualizer)
-        return "running"
+    def run(self) -> str:
+        if TERMINAL is True:
+            print('Running is finished. Transition to terminal state.')
+            return 'terminal'
+        print('AppState => running')
+        return 'running'
